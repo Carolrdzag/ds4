f21f7f15942df08e1bd39e3a02e9786189bc76ea
Carolrdzag <carolrdzag@gmail.com>
2025-09-24 09:22:07 -0500

    lab7

---
+++ Laboratorio7/Laboratorio72/JuegosDeDados.cs
@@ -0,0 +1,31 @@
+﻿ class JuegosDeDados
+ {
+    private Dado dado1, dado2, dado3;
+
+    public JuegosDeDados()
+    {
+        dado1 = new Dado();
+        dado2 = new Dado();
+        dado3 = new Dado();
+    }
+
+    public void Jugar()
+    {
+        dado1.Tirar();
+        dado1.Imprimir();
+        dado2.Tirar();
+        dado2.Imprimir();
+        dado3.Tirar();
+        dado3.Imprimir();
+        if (dado1.RetornarValor() == dado2.RetornarValor() &&
+            dado1.RetornarValor() == dado3.RetornarValor())
+        {
+            Console.WriteLine("Gano");
+        }
+        else
+        {
+            Console.WriteLine("Perdio");
+        }
+        Console.ReadKey();
+    }
+ }
