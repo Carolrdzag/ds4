1a91e4f7d0a930af19b882c7074a4bf2128096b3
Carolrdzag <carolrdzag@gmail.com>
2025-09-30 11:58:40 -0500

    Laboratorio 10 completo

---
+++ Laboratorio10/Laboratorio10/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Laboratorio10")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Laboratorio10")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4cf7f09b-d954-48a5-b27a-946c39a378a3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
