7dc3758ef1bd675155e75f11e533e6769f35bfba
Carolrdzag <carolrdzag@gmail.com>
2025-11-14 15:26:30 -0500

    Laboratorio 17 completo

---
+++ Laboratorio17/Laboratorio17/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Laboratorio17._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+        <div class="row">
+
+            <!-- GridView: Muestra datos en forma de tabla -->
+            <!-- DataSourceID="MyDataSource1": se conecta al SqlDataSource -->
+            <!-- AllowSorting y AllowPaging: permite ordenar y paginar -->
+            <!-- AutoGenerateEditButton: agrega boton para editar registros -->
+
+            <asp:GridView id="MyGridView" DataSourceID="MyDataSource1"  
+                AllowSorting="True" AllowPaging="True"
+                DataKeyNames="ProductID"
+                AutoGenerateEditButton="True"
+            Runat="Server"/>
+
+            <!-- Runat="Server": le dice a ASP.NET que ese control debe ser procesado en el servidor, no solo como HTML estático. -->
+
+            <asp:SqlDataSource ID="MyDataSource1" runat="server"
+                ConnectionString="data source=LAPTOP-PHQ6JRUD\SQLEXPRESS;initial catalog=northwind;persist security info=true;Integrated Security=SSPI;"
+                ProviderName="System.Data.SqlClient"
+                SelectCommand="SELECT ProductId, ProductName, UnitPrice From Products"
+                UpdateCommand="Update Products Set [ProductName]=@ProductName, [UnitPrice]=@UnitPrice Where [ProductId]=@ProductId">
+            </asp:SqlDataSource>
+          
+        </div>
+  
+
+</asp:Content>
+
+<!-- ConnectionString: detalles de conexión a SQL Server -->
+<!-- SelectCommand: consulta SQL para seleccionar datos -->
+<!-- UpdateCommand: consulta SQL para actualizar datos -->
---
+++ Laboratorio17/Laboratorio17/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Laboratorio17
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["db.Name"]; //error de conexión debido a que el connectionString tenia otro nombre distinto al que aparece en el WebConfig.
+            SqlConnection conexion = new SqlConnection(connString.ConnectionString);
+
+            //Uso de "using" para asegurar que el lector se cierre automaticamente cuando termine.
+
+            using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion)) //creación del objeto "cmd" para ejecutar un comando almacenado "SalesByCategory" en una conexión SQL.
+            {
+                cmd.CommandType = CommandType.StoredProcedure;  //especifica que el comando es un procedimiento almacenado, no una consulta SQL directa tipo SELECT.
+                cmd.Parameters.Add("@CategoryName", SqlDbType.VarChar).Value = "Seafood";  //agrega un parámetro al comando llamado "@CategoryName" de tipo VarChar y le asigna el valor "Seafood".
+                conexion.Open();  //abre la conexión a la base de datos, se usa antes del data reader.
+
+                using (SqlDataReader reader = cmd.ExecuteReader())  //ejecuta el procedimiento almacenado y devuelve los resultados en forma de tabla.
+                {
+                    if (reader.HasRows)  //verifica que el lector tiene al menos una fila y evita leer si no hay datos.
+                    {
+                        if (reader.Read())  //lee la primera fila del conjunto de resultados.
+                        {
+                            GridV.DataSource = reader;  //conectamos los datos al control GridView.
+                            GridV.DataBind();  //toma los datos del lector y los muestra en pantalla.
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
